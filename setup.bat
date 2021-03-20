fb12558fe2f5799f156b15280d5d012f525c47a7
Nick Johnson <57698887+b4iterdev@users.noreply.github.com>
2021-03-20 18:37:05 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,4 @@
 @echo off
-powershell
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 rem Run script as administrator
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
@@ -10,13 +7,13 @@ rem Change administrator password
 net user Administrator b4iterdev1111@
 mkdir C:\Bginfo\
 mkdir C:\Temp\
-powershell -command "Invoke-WebRequest -OutFile C:\Bginfo\bginfo.bgi -Uri https://github.com/b4iterdev/winserver2012r2personal/raw/main/bginfo.bgi"
-powershell -command "Invoke-WebRequest -OutFile C:\Bginfo\bginfo.zip -Uri https://download.sysinternals.com/files/BGInfo.zip"
-rem certutil.exe -urlcache -split -f "https://download.sysinternals.com/files/BGInfo.zip" C:\Bginfo\bginfo.zip
-rem certutil.exe -urlcache -split -f "https://github.com/b4iterdev/winserversetupscript/raw/main/bginfo.bgi" C:\Bginfo\bginfo.bgi
-powershell -command "Expand-Archive C:\Bginfo\bginfo.zip C:\Bginfo"
+powershell
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+Invoke-WebRequest -OutFile C:\Bginfo\bginfo.bgi -Uri https://github.com/b4iterdev/winserver2012r2personal/raw/main/bginfo.bgi
+Invoke-WebRequest -OutFile C:\Bginfo\bginfo.zip -Uri https://download.sysinternals.com/files/BGInfo.zip
+Expand-Archive C:\Bginfo\bginfo.zip C:\Bginfo
 C:\Bginfo\Bginfo64.exe "C:\Bginfo\bginfo.bgi" /timer 0
-powershell -command "Invoke-WebRequest -OutFile C:\Temp\WS2012R2Optimisations.ps1 -Uri https://github.com/b4iterdev/winserver2012r2personal/raw/main/WS2012R2Optimisations.ps1"
-rem certutil.exe -urlcache -split -f "https://github.com/b4iterdev/winserversetupscript/raw/main/WS2012R2Optimisations.ps1" C:\Temp\WS2012R2Optimisations.ps1
-powershell C:\Temp\WS2012R2Optimisations.ps1
+Invoke-WebRequest -OutFile C:\Temp\WS2012R2Optimisations.ps1 -Uri https://github.com/b4iterdev/winserver2012r2personal/raw/main/WS2012R2Optimisations.ps1
+C:\Temp\WS2012R2Optimisations.ps1
 pause
